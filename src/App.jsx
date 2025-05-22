bea1e801a6e9c7a2eb47d8e955f9117bbaaff2f7
NaveenReddy0427 <naveendeva909@gmail.com>
2025-05-22 12:07:54 +0530

    use queryclient for cache

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import SignUpCard from "@/components/organisms/auth/SignUpCard";
 import SignInCard from "@/components/organisms/auth/SignInCard";
 import Auth from "@/pages/Auth/Auth";
 import NotFound from "./pages/NotFound/NotFound";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+
+  const queryClient = new QueryClient()
+
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/auth/signup" element={<Auth><SignUpCard /></Auth>} />
         <Route path="/auth/signin" element={<Auth><SignInCard /></Auth>} />
 
         <Route path="/*" element={<NotFound/>} />
       </Routes>
+    </QueryClientProvider>
     </>
   );
 }
